182e6530b6170d55b61f9c7c0a675156cfd579d0
dmz866 <dmz866@hotmail.com>
2018-04-08 21:36:21 -0500

    Add project files.

---
+++ MyFirstApp/Controllers/EmployeeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MyFirstApp.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        //public IActionResult Index()
+        public string Index()
+        {
+            //return View();
+            return "String is from MVC Controller";
+        }
+    }
+}
